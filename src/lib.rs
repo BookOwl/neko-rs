d83f51aa1e439152f3a0a7015f20d2daf74bb914
BookOwl <stanleybookowl@gmail.com>
2017-05-03 10:56:06 -0400

    Start working on docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,14 @@
+//! A module for working with the [neko vm](http://nekovm.org/).
+//!
+//! This module contains two main parts.
+//!
+//! 1. The raw FFI bindings in the [raw submodule](raw/).
+//! 2. A safe, rusty wrapper in the top level.
+//!
+//! Unless you have a specific reason otherwise you should always use the safe wrapper instead of the raw bindings.
 pub mod raw {
+    //! This module contains the raw neko bindings generated using bindgen.
+    //! See http://nekovm.org/doc/vm for more information.
     #![allow(non_upper_case_globals)]
     #![allow(non_camel_case_types)]
     #![allow(non_snake_case)]
